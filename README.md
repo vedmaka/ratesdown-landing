bd0e8f5305e160faa3a9863eb2d4436a8c16d60a
Vedmaka <god.vedmaka@gmail.com>
2018-05-26 01:49:34 +0300

    optimized gulp with imagemin

---
+++ README.md
@@ -0,0 +1,13 @@
+## How to contribute
+
+### Requirements
+
+* NodeJS
+* NPM
+
+### Setup
+
+1. Run `npm install`
+2. Run `gulp default`
+3. Run `gulp watch`
+4. Change files in `src` folder, result will be complied into `docs` folder
--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,21 @@ const less = require('gulp-less');
 const gulpSequence = require('gulp-sequence');
 const fileInclude = require('gulp-file-include');
 const injectSvg = require('gulp-inject-svg');
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+const cssmin = require('gulp-cssmin');
+const rename = require('gulp-rename');
+const imagemin = require('gulp-imagemin');
+const changed = require('gulp-changed');
 
 const paths = {
     src: {
         js: 'src/js/**/*.js',
-        css: 'src/css/**/*.less',
+        js_full: 'src/js/**/*.js',
+        css: 'src/css/*.less',
+        css_full: 'src/css/**/*.less',
         html: 'src/*.html',
+        html_full: 'src/**/*.html',
         images: 'src/images/**.*'
     },
     dist: {
@@ -27,13 +36,21 @@ gulp.task('clean', () => {
 
 gulp.task('js', () => {
     return gulp.src(paths.src.js)
+        .pipe(sourcemaps.init())
         .pipe(babel())
+        .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest(paths.dist.js));
 });
 
 gulp.task('css', () => {
     return gulp.src(paths.src.css)
+        .pipe(sourcemaps.init())
         .pipe(less())
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest(paths.dist.css));
 });
 
@@ -48,11 +65,17 @@ gulp.task('html', () => {
 
 gulp.task('images', () => {
    return gulp.src(paths.src.images)
+       .pipe(changed(paths.dist.images))
+       .pipe(imagemin())
        .pipe(gulp.dest(paths.dist.images));
 });
 
 gulp.task('watch', () => {
-    gulp.watch('src/**/*.*', ['js', 'css', 'html', 'images']);
+    //gulp.watch('src/**/*.*', ['js', 'css', 'html', 'images']);
+    gulp.watch(paths.src.js_full, ['js']);
+    gulp.watch(paths.src.css_full, ['css']);
+    gulp.watch(paths.src.html_full, ['html']);
+    gulp.watch(paths.src.images, ['images']);
 });
 
 gulp.task('default', gulpSequence('clean',['js', 'css', 'html', 'images']));
